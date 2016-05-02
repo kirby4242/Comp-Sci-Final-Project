8bfe131d1fb82050c7562974bc0de496b32d1f93
kirby4242 <keegan.parkhurst@gmail.com>
2016-05-02 08:38:11 -0400

    The abstract class for cards

---
+++ CellGrowthSimulator2016/src/cellgrowthsimulator2016/Card.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author keeganparkhurst
+ */
+public abstract class Card {
+    String name;            //String for cardname
+    String imageAddress;    //String for image address
+    int id;                 //Card id
+    String rarity;          //Card rarity
+    
+    public abstract void play();    //Method that activates when you play the card
+}
