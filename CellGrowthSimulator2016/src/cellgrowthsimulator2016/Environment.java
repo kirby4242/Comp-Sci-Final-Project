806dd7634004f4efe83164f25f4839d530f883be
kirby4242 <keegan.parkhurst@gmail.com>
2016-05-04 08:35:28 -0400

    Adding environment class
    change anything you want. this had better not commit directly to master...

---
+++ CellGrowthSimulator2016/src/cellgrowthsimulator2016/Environment.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author keeganparkhurst
+ */
+public class Environment {
+    //Initialize variables
+    public int temp;
+    public int sunlight;
+    public int food;
+    public double pH;
+    public int predators;
+    public int pollution;
+    public double radiation;
+    
+    //default constructor
+    public Environment(){
+        temp = 20;
+        sunlight = 3;
+        food = 1;
+        pH = 7.0;
+        predators = 0;
+        pollution = 0;
+        radiation = 0.0;  
+    }
+    
+    
+}
