b991af4a54db537ac23e76e4a93a2890319f6ec7
BreadnButter <joshuamifogel@gmail.com>
2016-05-09 11:39:20 -0400

    Update CellGrowthSimulator2016.java

--- CellGrowthSimulator2016/src/cellgrowthsimulator2016/CellGrowthSimulator2016.java
+++ CellGrowthSimulator2016/src/cellgrowthsimulator2016/CellGrowthSimulator2016.java
@@ -16,6 +16,7 @@ public class CellGrowthSimulator2016 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Window w = new Window(1280);
     }
     
 }
